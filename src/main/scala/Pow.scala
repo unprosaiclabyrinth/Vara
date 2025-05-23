81e7298240b38ee33daf93b2d82791f53321c7c5
unprosaiclabyrinth <hdongr2@uic.edu>
2025-05-23 16:58:29 -0500

    Hijack and override apply for custom

--- src/main/scala/Pow.scala
+++ src/main/scala/Pow.scala
@@ -1,12 +1,4 @@
 final case class Pow(base: Expr, index: Expr) extends Expr:
-  def apply(base: Expr, index: Expr): Expr = (base, index) match
-    case (_, Const(0D)) | (Const(1D), _) => Const(1D)
-    case (b, Const(1D)) => b
-    case (Const(0D), _) => Const(0D)
-    case (Const(b), Const(i)) => Const(math.pow(b, i))
-    case (Pow(u, v), _) => Pow(u, Mul2(v, index))
-    case _ => Pow(base, index)
-
   override def eval(using env: Env): Expr = base.eval |: index.eval
 
   override def equals(that: Any): Boolean = that match
@@ -14,3 +6,12 @@ final case class Pow(base: Expr, index: Expr) extends Expr:
     case _ => false
 
   override def hashCode: Int = (base, index).hashCode
+
+object Pow:
+  def apply(base: Expr, index: Expr): Expr = (base, index) match
+    case (_, Const(0D)) | (Const(1D), _) => Const(1D)
+    case (b, Const(1D)) => b
+    case (Const(0D), _) => Const(0D)
+    case (Const(b), Const(i)) => Const(math.pow(b, i))
+    case (Pow(u, v), _) => new Pow(u, Mul2(v, index))
+    case _ => new Pow(base, index)
