42885c3f7fdeca474d36aba5918638904d4bce95
unprosaiclabyrinth <hdongr2@uic.edu>
2025-05-23 17:24:51 -0500

    Print int values without trailing 0's

--- src/main/scala/Const.scala
+++ src/main/scala/Const.scala
@@ -1,8 +1,10 @@
 final case class Const(value: Double) extends Expr:
   override def eval(using env: Env): Expr = this
-  
+
+  override def toString: String = s"Const(${if value.isWhole then value.toInt else value})"
+
   override def equals(that: Any): Boolean = that match
     case Const(thatValue) => value == thatValue
     case _ => false
-    
+
   override def hashCode: Int = value.hashCode
