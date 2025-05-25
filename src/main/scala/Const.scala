1977b4386b512626a414caffdafb6ba66c9ecdf2
unprosaiclabyrinth <hdongr2@uic.edu>
2025-05-24 20:45:32 -0500

    Add case for primitive Numbers in Const equals

--- src/main/scala/Const.scala
+++ src/main/scala/Const.scala
@@ -5,6 +5,7 @@ final case class Const(value: Double) extends VaraExpr:
 
   override def equals(that: Any): Boolean = that match
     case Const(thatValue) => value == thatValue
+    case n: java.lang.Number => value == n.doubleValue
     case _ => false
 
   override def hashCode: Int = value.hashCode
