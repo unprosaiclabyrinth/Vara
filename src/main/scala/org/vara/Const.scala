693f81ea3134590889f0fdb870763102057fcb8c
unprosaiclabyrinth <hdongr2@uic.edu>
2025-05-27 23:55:28 -0500

    Print consts to 5 decimal places

--- src/main/scala/org/vara/Const.scala
+++ src/main/scala/org/vara/Const.scala
@@ -6,8 +6,8 @@ final case class Const(value: Double) extends VaraExpr:
   override def eval(using env: VaraEnv): VaraExpr = this
 
   override def toString: String = {
-    val df = new DecimalFormat("#.####")
-    s"${if value.isWhole then value.toInt else df.format(value)}"
+    val df5 = new DecimalFormat("#.##5##")
+    s"${if value.isWhole then value.toInt else df5.format(value)}"
   }
 
   override def equals(that: Any): Boolean = that match
