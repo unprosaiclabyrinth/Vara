9e5cdf3ee3d298a1750497f6661bb5e6f681a89f
unprosaiclabyrinth <hdongr2@uic.edu>
2025-07-13 15:01:07 -0500

    Update with example

--- src/main/scala/org/vara/Calculator.scala
+++ src/main/scala/org/vara/Calculator.scala
@@ -1,9 +1,22 @@
 package org.vara
-import VaraExpr.fromInt
+import VaraExpr.*
 
 object Calculator:
+  private def f(i: VaraExpr): VaraExpr =
+    // complex computation
+    (1 to 5).foldLeft(e(0))(
+      (acc, _) => acc +# i
+    )
+    /*
+     sum = 0;
+     for (j = 1; j <= 5; j++) {
+         sum += i;
+     }
+     return sum;
+     */
+
   @main
   def compute(): Unit =
-    val expr: VaraExpr = (("a" +# "b") *# ("f" + "g") #: -2) /# ("b" *# "c" +# "d")
-    expr.printAST()
-    println(Const(0.5))
+    val e = f("x")
+    val f5 = put ("x" -> 5) in e
+    println(f5)
