45c7a0f8fcd0b0f081573d3c27ae01b673e7d33b
unprosaiclabyrinth <hdongr2@uic.edu>
2025-07-17 10:23:53 -0500

    Add tests for exp

--- src/test/scala/PowerSpec.scala
+++ src/test/scala/PowerSpec.scala
@@ -1,15 +1,18 @@
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
+import org.vara.VaraExpr.fromInt
 import org.vara.Variable
 
 class PowerSpec extends AnyWordSpec with Matchers:
   "Exponentiation" should {
-    "follow power laws" in {
+    "follow laws of indices" in {
       "a"#:"m" *# "a"#:"n" should equal ("a"#:("m" +# "n"))
       "a"#:"m" /# "a"#:"n" should equal ("a"#:("m" -# "n"))
+      "a"#:"k" /# "a" should equal ("a"#:("k" -# 1))
       ("a"#:"m")#:"n" should equal ("a"#:("m" *# "n"))
     }
-//    "distribute over a product" in {
-//      ("a" *# "b")#:"m" should equal ("a"#:"m" *# "b"#:"m")
-//    }
+    "distribute constant powers over a product" in {
+      ("a" *# "b")#:10 should equal ("a"#:10 *# "b"#:10)
+      "a"#:(2025 +# "k") *# ("a" *# "b")#:(-2025) should equal ("a"#:"k" /# "b"#:2025)
+    }
   }
