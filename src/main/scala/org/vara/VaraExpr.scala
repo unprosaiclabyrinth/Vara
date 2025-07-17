49abfc67de812735718486f0d90a407b8ce135bf
unprosaiclabyrinth <hdongr2@uic.edu>
2025-07-16 22:19:55 -0500

    Define a replace API

--- src/main/scala/org/vara/VaraExpr.scala
+++ src/main/scala/org/vara/VaraExpr.scala
@@ -54,7 +54,7 @@ trait VaraExpr:
         case Pow(a, b) => a.contains(e) || b.contains(e)
         case _ => false // this==e is already checked
     }
-
+  
 object VaraExpr:
   type VaraEnv = Map[String, VaraExpr]
   given emptyEnv: VaraEnv = Map.empty
@@ -63,6 +63,12 @@ object VaraExpr:
 
   implicit inline def fromInt(i: Int): VaraExpr = Const(i.toDouble)
 
+  infix inline def e(inline s: String): VaraExpr = s
+
+  infix inline def e(inline i: Int): VaraExpr = i
+
+  infix inline def e(inline d: Double): VaraExpr = d
+
   /* put API */
   case class Put(bindings: (String, VaraExpr)*):
     infix def in(expr: VaraExpr): VaraExpr =
@@ -70,9 +76,37 @@ object VaraExpr:
       expr.eval(using env ++ bindings.toMap)
 
   infix def put(bindings: (String, VaraExpr)*): Put = Put(bindings *)
+  
+  /* replace API */
+  case class ReplaceWith(old: VaraExpr, replacement: VaraExpr):
+    infix def in(expr: VaraExpr): VaraExpr =
+      if !expr.contains(old) then expr
+      else if old == expr then replacement
+      else expr match
+        case Add(sum*) =>
+          if old.isInstanceOf[Add] && old.asInstanceOf[Add].terms.forall(sum contains) then
+            val subsum = old.asInstanceOf[Add].terms
+            sum.filterNot(subsum contains).foldLeft(replacement)(
+              (acc, term) => acc +# term
+            )
+          else if sum contains old then sum.filterNot(_ == old).foldLeft(replacement) {
+            (acc, term) => acc +# term
+          }
+          else sum.foldLeft(e(0))((acc, term) => acc +# in(term))
+        case Mul(prod*) =>
+          if old.isInstanceOf[Mul] && old.asInstanceOf[Mul].terms.forall(prod contains) then
+            val subprod = old.asInstanceOf[Mul].terms
+            prod.filterNot(subprod contains).foldLeft(replacement)(
+              (acc, term) => acc *# term
+            )
+          else if prod contains old then prod.filterNot(_ == old).foldLeft(replacement) {
+            (acc, term) => acc *# term
+          }
+          else prod.foldLeft(e(1))((acc, term) => acc *# in(term))
+        case Pow(a, b) => in(a) #: in(b)
+        case _ => expr // old==expr is already check
 
-  infix inline def e(inline s: String): VaraExpr = s
-
-  infix inline def e(inline i: Int): VaraExpr = i
-
-  infix inline def e(inline d: Double): VaraExpr = d
+  case class Replace(old: VaraExpr):
+    infix def withExpr(replacement: VaraExpr): ReplaceWith = ReplaceWith(old, replacement)
+    
+  infix def replace(old: VaraExpr): Replace = Replace(old)
